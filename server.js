c6ad97a10610f13fa37755a945ba9e0b32b1f584
Jack Frost <kodeandplay@gmail.com>
2016-03-14 15:48:22 -0700

    Space

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 app.get('/about', middleware.requireAuthentication, function(req, res) {
 	res.send('About Us');
 });
- 
+  
 app.listen(PORT, function() {
 	console.log('Server listening on port %d', PORT);
 });
